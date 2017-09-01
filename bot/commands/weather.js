1f65188f3d24704f98574ab89579666073dd21b5
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-01 01:43:18 +0100

    Refactoring and added location
    Refactored the logic to fetch weather data and added google maps api to get location based on coordinates (reverse geolocation)

--- bot/commands/weather.js
+++ bot/commands/weather.js
@@ -1,30 +1,48 @@
-require('dotenv').config();
+const { processWeatherCommand } = require('../services/weather');
+const errors = require('../errors/index');
 
-const knex = require('./../../server/db/connection');
-const rp = require('request-promise');
-
-const weatherApiKey = process.env.WEATHER_API_KEY;
-
-const getCurrentWeather = (lat, lng) => {
-    const options = {
-        uri: `https://api.darksky.net/forecast/${weatherApiKey}/${lat},${lng}`,
-        json: true,
-    };
-    return rp(options)
-            .then(res => `It's currently ${((Number.parseFloat(res.currently.temperature) - 32) / 1.8).toFixed(2)} ºC.`)
-            .catch(err => console.log(err));
-};
-
-const weather = (userId, coords = null, alias = null) => {
-    if (coords) {
-        // get weather using coordinates
-        return getCurrentWeather(coords[0], coords[1]);
-    } else if (alias) {
-        // search for alias location in the database using userId + alias
-        const coordsFromDb = knex('users').where('telegram_id', '=', userId).join('weather', 'weather.user_id');
-        return getCurrentWeather(coordsFromDb[0], coordsFromDb[1]);
+const weather = (bot, msg, match) => {
+    const userId = msg.from.id;
+    // Remove white-spaces
+    const params = match[1].split(' ').filter(str => str !== '');
+    // console.log(params);
+    if (!params || params.length === 0) {
+        // config for location request
+        const options = {
+            parse_mode: 'Markdown',
+            reply_markup: JSON.stringify({
+                one_time_keyboard: true,
+                keyboard: [
+                    [{ text: 'Share my location', request_location: true }],
+                    ['Cancel'],
+                ],
+            }),
+        };
+        // ask user for location
+        return bot.sendMessage(msg.chat.id, 'Please share your location', options)
+            .then(() => {
+                // get weather
+                bot.once('location', msgLocation => processWeatherCommand(msgLocation.location.latitude, msgLocation.location.longitude)
+                                        .then(res => bot.sendMessage(msgLocation.chat.id, res, { parse_mode: 'Markdown' }))
+                                        .catch(() => bot.sendMessage(msgLocation.chat.id, errors.weather.server)));
+            })
+            .catch(() => bot.sendMessage(msg.chat.id, errors.weather.server));
+    } else if (params.length === 2 && params.every(val => !isNaN(val))) {
+        // two numeric arguments (params = [lat, lng])
+        return processWeatherCommand(params[0], params[1])
+            .then(res => bot.sendMessage(msg.chat.id, res, { parse_mode: 'Markdown' }))
+            .catch(() => bot.sendMessage(msg.chat.id, errors.weather.server));
     }
-    return 'Error';
+    // else if (params.length === 1) {
+    //     // single argument (params = alias)
+    //     // return getCurrentWeather(userId, null, params)
+    //     //     .then(res => bot.sendMessage(msg.chat.id, res), { parse_mode: 'Markdown' })
+    //     //     .catch(() => bot.sendMessage(msg.chat.id, errors.weather.server));
+    //     // TODO: error needs to specify if the problem is: 1. server error; 2. user does not exist; 3. alias is not defined on the db
+    // }
+    // Error in command arguments
+    // Send error message to user
+    return bot.sendMessage(msg.chat.id, errors.weather.params);
 };
 
 module.exports = weather;
