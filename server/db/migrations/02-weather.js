c3a8e13adfb972f68c93f463d052d700cdb837fb
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-07 02:26:25 +0100

    Added notification related columns
    And removed "units" column since this feature is not a priority

--- server/db/migrations/02-weather.js
+++ server/db/migrations/02-weather.js
@@ -3,8 +3,10 @@ exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('weather', (t
     table.string('alias').notNullable();
     table.integer('lat').notNullable();
     table.integer('lng').notNullable();
-    table.enu('unit', ['c', 'f']).notNullable();
-    table.integer('user_id').references('users.id');
+    // table.enu('unit', ['c', 'f']).notNullable();
+    table.integer('user_id').references('users.telegram_id');
+    table.boolean('notifications').defaultTo(false);
+    table.timestamp('notifications_time');
     table.timestamp('createdAt').notNullable().defaultTo(knex.raw('now()'));
     table.timestamp('updatedAt').defaultTo(knex.raw('now()'));
 });
