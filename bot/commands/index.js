f9b928a635e93b8edec095866487b89ab5c255df
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-07 02:21:24 +0100

    Added command handler

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -1,5 +1,19 @@
+const bot = require('./../bot.js');
+console.log(bot);
+
 const commands = {};
 
 commands.weather = require('./weather');
+
+commands.handleText = (bot, msg) => {
+    const text = msg.text;
+    if (/^\/weather(.*)/.test(text)) {
+        const match = text.match(/^\/weather(.*)/);
+        return commands.weather(bot, msg, match);
+    }
+    return bot.sendMessage(msg.chat.id, 'Command not found');
+};
+
+
 
 module.exports = { commands };
