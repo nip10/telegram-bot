a98c12459ea6f74ad9f265b4700c513100cdddde
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-08 01:25:57 +0100

    Add weather alias feature
    This feature allows the user to save a location (or multiple locations) in the db so he can fetch it later by using an alias. For example, /weather home, or /weather work

--- bot/services/weather.js
+++ bot/services/weather.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Promise = require('bluebird');
 const rp = require('request-promise');
+const knex = require('../../server/db/connection');
 
 const weatherApiKey = process.env.WEATHER_API_KEY;
 const googlegeolocationApiKey = process.env.GEOLOCATION_API_KEY;
@@ -38,6 +39,18 @@ const getLocationFromCoords = (lat, lng) => {
     return rp(options).then(res => res.results[0].address_components[0].long_name).catch(err => console.log(err));
 };
 
+const getCoordsFromLocation = (city, country) => {
+    const options = {
+        uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${city},${country}&key=${googlegeolocationApiKey}`,
+        json: true,
+    };
+    return rp(options).then(res => res.results[0].geometry.location).catch(err => console.log(err));
+};
+
+const getAliasCoords = (userId, alias) => knex('weather').select('lat', 'lng').where('user_id', '=', userId).andWhere('alias', '=', alias);
+
+const saveAliasCoords = (userId, alias, lat, lng) => knex('weather').insert({ alias, lat, lng }).where('user_id', '=', userId);
+
 const processWeatherCommand = (lat, lng) => {
     const promises = [getCurrentWeather(lat, lng), getLocationFromCoords(lat, lng)];
     return Promise.all(promises.map(promise => promise.reflect()))
@@ -54,4 +67,4 @@ const processWeatherCommand = (lat, lng) => {
         });
 };
 
-module.exports = { processWeatherCommand };
+module.exports = { processWeatherCommand, getAliasCoords, saveAliasCoords, getCoordsFromLocation };
