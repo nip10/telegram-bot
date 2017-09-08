e0ef8676d95c29daa868545b703fa3ef8b54105b
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-08 01:23:55 +0100

    refactor after circular dependencies fix
    Since the circular dependencies issue was fixed, I no longer need to bind "bot" to the message handler

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -1,19 +1,15 @@
 const bot = require('./../bot.js');
-console.log(bot);
 
 const commands = {};
 
 commands.weather = require('./weather');
 
-commands.handleText = (bot, msg) => {
+commands.handleText = (msg) => {
     const text = msg.text;
     if (/^\/weather(.*)/.test(text)) {
-        const match = text.match(/^\/weather(.*)/);
-        return commands.weather(bot, msg, match);
+        return commands.weather(bot, msg);
     }
     return bot.sendMessage(msg.chat.id, 'Command not found');
 };
 
-
-
-module.exports = { commands };
+module.exports = commands;
